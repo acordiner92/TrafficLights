043741073fff52a777d5537362be8ce7e91f5182
Andrew C <acacwork99@gmail.com>
2017-04-08 23:21:54 +1000

    Changed values to consts

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import TrafficController from "./trafficController";
 import { DIRECTION } from "./TrafficLightConstants";
 
+const SIM_DURATION = 30;
+const YELLOW_LIGHT_DURATION = 0.5;
+const LIGHT_DURATION = 5;
+
 let trafficController = new TrafficController();
 let time = 0;
-while (time < 30 * 60) {
+while (time < SIM_DURATION) {
   trafficController.executeTrafficLightChange();
-  time = time + 30; // 30s for change to yellow
-  time = time + 60 * 5; // 5min light change
+  time = time + YELLOW_LIGHT_DURATION; // 30s for change to yellow
+  time = time + LIGHT_DURATION; // 5min light change
 }
 
